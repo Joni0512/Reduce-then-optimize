1fe123a92e8c3fe155fc5899394deb7bbfd76fe9
j_wiltfang <wiltfang.jasper@gmail.com>
2026-02-12 09:18:38 -0500

    update legacy shell

--- rtv_solver/run_simulation_legacy.sh
+++ rtv_solver/run_simulation_legacy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # shell script to run main.py (possibly does not work atm, but main.py has feature parity)
+# TODO not sure whether this script currently works, specifically argparse might be corrupted due to integration of config to get it running quickly
 RH=0 #rolling horizen
 INTERVAL=600
 echo "Debug Run"
-mkdir -p "../output_format/$RH/$INTERVAL/"
-nohup python main_legacy.py --server_url "http://127.0.0.1:5001/" --input_file "../inputs/localDB_payload_oct.pkl" --out_put_dir "../output_format/$RH/$INTERVAL/" --interval $INTERVAL --rh_factor $RH --max_cardinality 4  > ../output_format/debug.out 2> ../output_format/debug.err &
-echo $! > ../output_format/processID-$RH-$INTERVAL
+mkdir -p "../outputs/legacy/$RH/$INTERVAL/"
+python main_legacy.py --server_url "http://127.0.0.1:5001/" --input_file "../inputs/localDB_payload_oct.pkl" --out_put_dir "../outputs/legacy/$RH/$INTERVAL/" --interval $INTERVAL --rh_factor $RH --max_cardinality 4  > ../outputs/legacy/debug.out 2> ../outputs/legacy/debug.err &
+echo $! > ../outputs/legacy/processID-$RH-$INTERVAL<
